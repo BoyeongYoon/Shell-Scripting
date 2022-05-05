a75294b7f64f1004152492963059ababe16aa69a
Boyeong Yoon <boyeong.nancy.yoon@gmail.com>
2022-05-05 16:00:22 -0400

    Update PasswordValidation.sh

--- PasswordValidation.sh
+++ PasswordValidation.sh
@@ -11,18 +11,24 @@ PasswordValidation(){
 
 
 
-        # Asks an user to create a password
+        # Asks an user to create a password and store it in the variable PASSWORD
         echo -n "Enter your password: "
         read PASSWORD
 
 
 
-        # Checks whether the password is valid
+        # Checks whether the password met all the conditions
+        # (1) at least 8 characters
+        # (2) at least 1 number
+        # (3) at least 1 uppercase character
+        # (4) at least 1 lowercase chracter
+        # If the password satisfied all the conditions then prints 'VALID PASSWORD' else 'INVALID PASSWORD'
+        
         VALIDATION=`echo $PASSWORD | \
                 grep '[[:alnum:]]' | \
                 grep '[[:lower:]]' | \
                 grep '[[:upper:]]'`
-
+        
         if [ $VALIDATION ] && [ ${#PASSWORD} -ge 8 ]
         then
                 echo -n "...VALID PASSWORD"
@@ -33,7 +39,9 @@ PasswordValidation(){
 
 
 
-        # Prints the details of validation criteria
+        # Prints the details of validation criteria if the password met the condition then prints 'V' else 'X'
+        # There are four conditions - whether its length is greater than 8, it includes numeric numbers, uppercase letter or lowercase letter
+        
         if [ ${#PASSWORD} -ge 8 ]; then MORETHAN8CHRS="V"; else MORETHAN8CHRS="X"; fi
         if [ `echo $PASSWORD | grep '[[:digit:]]'` ]; then DIGIT="V"; else DIGIT="X"; fi
         if [ `echo $PASSWORD | grep [[:upper:]]` ]; then UPPER="V"; else UPPER="X"; fi
